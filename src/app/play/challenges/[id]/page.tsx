b61864a075fa20718e1cdb9be85beb3d5a2d2324
Kristian Cabading <kcabading@gmail.com>
2023-05-28 17:01:52 +0800

    Fixed base url issue on challenge component

--- src/app/play/challenges/[id]/page.tsx
+++ src/app/play/challenges/[id]/page.tsx
@@ -1,19 +1,25 @@
-// import { useState, useEffect } from 'react'
 import Game from '@/app/components/game/Game'
-// import useCurrentUser from '@/app/hooks/useCurrentUser'
-// import { useRouter,useSearchParams,usePathname } from 'next/navigation'
 import { headers } from 'next/headers';
+import { URL } from 'url';
 
 
-async function getGameOption(id: string) {
-    const res = await fetch( process.env.NEXTAUTH_URL + `/api/challenges/${id}`);
+
+async function getGameOption() {
+
+    const headersList = headers();
+    const domain = headersList.get('host') || "";
+    const fullUrl = headersList.get('referer') || "";
+    const urlPaths: string[] = fullUrl.split('/')
+    let challegeId = urlPaths[urlPaths.length - 1]
+    const baseUrl = new URL('/', fullUrl)
+    const res = await fetch( baseUrl + `/api/challenges/${challegeId}`);
     // Recommendation: handle errors
     if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error('Failed to fetch data');
     }
    
-    return res.json();
+    return res.json()
 }
 
 
@@ -21,18 +27,10 @@ async function getGameOption(id: string) {
 const Challenge = async function() {
 
     console.log('challenge render')
-    const headerList = headers()
-    const pathname = headerList.get('x-invoke-path')!
-    const urlPaths: string[] = pathname.split('/')
-
-    if (urlPaths.length !== 4) throw new Error('Invalid parameter!')
-    const challengeId = urlPaths[3]
-    
-    let gameOptions = await getGameOption(urlPaths[3])
-    
+    let gameOptions = await getGameOption()
     return (
         <>  
-            <h1 className='font-bold text-xl mb-5'>Challenge#: {challengeId}</h1>
+            {/* <h1 className='font-bold text-xl mb-5'>Challenge#: {challengeId}</h1> */}
             <Game startOption={true} {...gameOptions} />   
         </>
     )
