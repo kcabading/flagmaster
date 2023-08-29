c0d74610f28c3d740955ead8a754535fd86e5912
Kristian Cabading <kcabading@gmail.com>
2023-08-29 23:00:10 +0800

    adjusted speech recognition check

--- src/hooks/useSpeechBrowserCompatibility.ts
+++ src/hooks/useSpeechBrowserCompatibility.ts
@@ -18,7 +18,7 @@ function getBrowserName(userAgent:any) {
     } else if (userAgent.includes("Edg")) {
       // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70"
       return "Microsoft Edge (Chromium)";
-    } else if (userAgent.includes("Chrome")) {
+    } else if (userAgent.includes("Chrome") || userAgent.includes("CriOS")) {
       // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
       return "Google Chrome or Chromium";
     } else if (userAgent.includes("Safari")) {
